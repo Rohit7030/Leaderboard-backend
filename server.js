6b98821a12d418d0b87088172b88ff9dcbd65c88
Rohit Khedkar <rohitkhedkar5060@gmail.com>
2025-07-14 17:18:02 +0530

    Frontend URL update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", // for local dev (change to frontend URL in production)
+  origin: "https://leaderboard-frontend-tau.vercel.app", // for local dev (change to frontend URL in production)
   credentials: true,
 }));
 
